89f02ce0c858825c56125bc9d67f00c65dcba08a
Vithop <vithuransoccer1@gmail.com>
2021-03-25 00:37:37 -0400

    simplified mp4 to wav

--- server/flask/siren_sense_predict.py
+++ server/flask/siren_sense_predict.py
@@ -1,18 +1,24 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
-import numpy as np
+import tensorflow as tf
+import soundfile as sf
 import pandas as pd
+import numpy as np
 import librosa
+import pprint
+import random
+import os
+import io
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import os
-from pydub import AudioSegment
-import random
+# from pydub import AudioSegment
 from sklearn.preprocessing import LabelEncoder
-import tensorflow as tf
-import pprint
 from tensorflow.keras.utils import to_categorical
+
+
+app = Flask(__name__)
+CORS(app)
 
 class Sense:
     max_pad_len = 174
@@ -24,7 +30,7 @@ class Sense:
     def __init__(self):
 
         # Convert into a Panda dataframe 
-        # featuresdf = pd.read_csv("server/flask/model/featuresdf.csv")
+        # self.featuresdf = pd.read_csv("server/flask/model/featuresdf.csv")
         self.featuresdf = pd.read_csv("model/featuresdf.csv")
 
         y = np.array(self.featuresdf.class_label.tolist())
@@ -34,12 +40,14 @@ class Sense:
         self.le = LabelEncoder()
         to_categorical(self.le.fit_transform(y))
 
-
-        # model = tf.keras.models.load_model('server/flask/model/siren_sense_model.hdf5')
+        # self.model = tf.keras.models.load_model('server/flask/model/siren_sense_model.hdf5')
         self.model = tf.keras.models.load_model('model/siren_sense_model.hdf5')
+        # self.sess = tf.compat.v1.keras.backend.get_session()
+        # self.graph = tf.Graph
     def predict(self, file_name):
         try:
             audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast') 
+            # audio, sample_rate = sf.read(file_name.stream()) 
             mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
             pad_width = self.max_pad_len - mfccs.shape[1]
             mfccs = np.pad(mfccs, pad_width=((0, 0), (0, pad_width)), mode='constant')
@@ -50,16 +58,15 @@ class Sense:
         
         prediction_feature = mfccs.reshape(1, self.num_rows, self.num_columns, self.num_channels)
 
-    
-        predicted_vector = self.model.predict_classes(prediction_feature)
+
+        predicted_vector = np.argmax(self.model.predict(prediction_feature), axis=-1)
         predicted_classes = self.le.inverse_transform(predicted_vector) 
         # print("The predicted class is:", predicted_classes[0], '\n') 
 
         return predicted_classes[0]
 
 
-app = Flask(__name__)
-CORS(app)
+
 sense = Sense()
 
 # Quick Test 
@@ -75,7 +82,7 @@ def siren_sense_predict_wav():
     
 
     audio_file = request.files["file"]
-    file_name = str(random.randint(0,1000000))
+    file_name = str(random.randint(0,1000000))+".wave"
     audio_file.save(file_name)
 
     predicted_class = sense.predict(file_name)
@@ -89,11 +96,10 @@ def siren_sense_predict_wav():
 def siren_sense_predict_mp4():
         
     audio_file = request.files["file"]
-    file_name = str(random.randint(0,1000000))
-    sound = AudioSegment.from_file(audio_file, "mp4")
-    sound.export(file_name, format="wav")
+    file_name = str(random.randint(0,1000000))+".mp4"
+    audio_file.save(file_name)
     print("saved file")
-
+    # audio, sample_rate = sf.read(audio_file.stream()) 
     data = {"predicted_class": sense.predict(file_name)}
 
     os.remove(file_name)
